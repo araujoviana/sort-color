f72c5268f59d3773880c6cd4be655b46747a3eee
araujoviana <63423848+araujoviana@users.noreply.github.com>
2025-01-06 22:59:41 -0300

    Filter for bitmaps in user's folder

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,11 @@ import System.Environment
 import System.Directory
 import System.FilePath
 import Data.Char
+import qualified Data.List as L
 import Control.Monad
 import Data.Either
 
-type FolderPath = String
+type FolderPath = String -- Path to the folder containing the files
 type Color = String
 data Order = Asc | Desc deriving (Show, Read)
 
@@ -17,14 +18,14 @@ usage = "Usage: ./sort-color <folder path> <color> <order>"
 main :: IO ()
 main = do
   args <- getArgs
-
-  when (isLeft $ isolateArgs args) $ mapM_ putStrLn ["Invalid arguments", usage]
+  when (isLeft $ isolateArgs args ) $ mapM_ putStrLn ["Invalid arguments", usage]
   let Right (folder, color, order) = isolateArgs args
 
-  -- Select all the files in the folder that have the .bmp extension
-  bmpFiles <- filterM (doesFileExist . (folder ++)) . filter ((== ".bmp") . takeExtension) <$> listDirectory folder
+  files <- listDirectory folder -- NOTE This will throw an exception if the folder does not exist
+  bitmaps <- filterM (return . L.isSuffixOf ".bmp") 
 
-  print <$> bmpFiles
+  -- TODO Check if internal contents of bitmap are valid
+
 
   return ()
 
